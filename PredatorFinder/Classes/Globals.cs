532059a363c5f5bab8f4d3e6fb8951ca218ebc91
Whiroo <falweek@outlook.com>
2019-07-20 19:41:29 +0300

    Added Globals Variable

---
+++ PredatorFinder/Classes/Globals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredatorFinder.Classes
+{
+    /// <summary>
+    /// Constain global variables
+    /// </summary>
+    class Globals
+    {
+        public static string WorkStatus = "";
+        public static int Threads = 0;
+        public static ArrayList Source = new ArrayList();
+        public static int TimeOut = 7000;
+        public static int GoodDomain = 0;
+        public static int BadDomain = 0;
+
+    }
+}
