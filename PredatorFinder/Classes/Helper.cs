e865ae30a7949dd19523c8f8c453be2a77380c4c
Whiroo <falweek@outlook.com>
2019-07-20 19:52:27 +0300

    Added method for dev help

---
+++ PredatorFinder/Classes/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PredatorFinder.Classes
+{
+    class Helper
+    {
+        public static object Statistic()
+        {
+            object roster = "";
+
+            roster += "Source: " + Globals.Source.Count.ToString() + Environment.NewLine;
+            roster += "Good: " + Globals.GoodDomain.ToString() + Environment.NewLine;
+            roster += "Bad: " + Globals.GoodDomain.ToString() + Environment.NewLine;
+            roster += "Left" + (Globals.Source.Count - (Globals.GoodDomain + Globals.BadDomain)).ToString();
+
+            return roster;
+        }
+
+        public void SaveText(string file, string text)
+        {
+            try
+            {
+                var write = new StreamWriter(file,true);
+                write.WriteLineAsync(text);
+                write.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public static string OpenDialog()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "*.txt | *.txt";
+            dialog.ShowDialog();
+            return dialog.FileName;
+        }
+
+        public static object[] LoadData(string filename)
+        {
+            var roster = new ArrayList();
+            roster.AddRange(File.ReadAllLines(filename));
+            return roster.ToArray();
+        }
+    }
+}
